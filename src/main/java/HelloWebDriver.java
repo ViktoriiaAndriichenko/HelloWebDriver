e6143c33fd15e1c69c5693930ef6ed734100afdf
Vika test <Vika0979444756@gmail.com>
2024-02-20 00:33:21 +0200

    webDr_v3

--- src/main/java/HelloWebDriver.java
+++ src/main/java/HelloWebDriver.java
@@ -2,11 +2,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -15,31 +16,36 @@ public class HelloWebDriver {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev");
-        Thread.sleep(5000);
+        Thread.sleep(3000);
        // WebElement downloadButton = driver.findElement(By.id("main_navbar"));
        // downloadButton.sendKeys("selenium java");
         WebElement downloadButton = driver.findElement(By.xpath("//*[@id='main_navbar']/ul/li[2]/a/span"));
         downloadButton.click();
-        Thread.sleep(5000);
+        Thread.sleep(3000);
 
         TakesScreenshot scrShot =((TakesScreenshot)driver);
         File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
         try {
-            FileUtils.copyFile(SrcFile, new File("C:\\Users\\Viktoriia_Andriichen\\OneDrive - EPAM\\Vika\\Screenshotsu\\Screenshot.png"));
+            FileUtils.copyFile(SrcFile, new File("C:\\Screenshots\\Screenshot_Chrom.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
         driver.quit();
 
-       driver = new EdgeDriver();
-        Thread.sleep(5000);
+        driver = new FirefoxDriver();
        driver.get("https://www.selenium.dev");
-       Thread.sleep(2000);
+       Thread.sleep(3000);
+        downloadButton = driver.findElement(By.xpath("//*[@id=\"main_navbar\"]/ul/li[2]/a"));
+        downloadButton.click();
+       Thread.sleep(3000);
+        scrShot =((TakesScreenshot)driver);
+        SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(SrcFile, new File("C:\\Screenshots\\Screenshot_Mozilla.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
        driver.quit();
 
-//        driver = new FirefoxDriver();
-//        driver.get("https://www.selenium.dev");
-//        Thread.sleep(2000);
-//        driver.quit();
     }
 }
